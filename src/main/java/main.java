5d29992b64ad6a363c0e446eb1f25f470b201a32
Hule-Elev <oulstein@gmail.com>
2017-09-28 22:06:12 +0200

    a

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,18 +1,16 @@
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
-import com.dropbox.core.v2.files.FileMetadata;
 import com.dropbox.core.v2.files.ListFolderResult;
 import com.dropbox.core.v2.files.Metadata;
 import com.dropbox.core.v2.users.FullAccount;
-
 import javax.swing.*;
 import java.io.*;
 
 public class main {
     private static final String ACCESS_TOKEN = "gJKkMgm0o8AAAAAAAAAAYSuQZFy2plTSmSAt02gsXVAxn2gTn-qVBaqIMvdZwy8q";
 
-    public static void main(String args[]) throws DbxException {
+    public static void main(String [] args) throws DbxException {
         byte[] b = {65,66,67,68,69};;
 
         DbxRequestConfig config = new DbxRequestConfig("dropbox", "en_US");
@@ -25,19 +23,12 @@ public class main {
         while (true) {
             for (Metadata metadata : result.getEntries()) {
                 System.out.println(metadata.getPathLower());
-                if (metadata.getPathLower().equals("/text.txt")) {
-                    client.files().delete("/text.txt");
-                }
                 try {
-                    InputStream in = new FileInputStream("C:\\Users\\Hule-Elev\\Documents\\output.txt");
-                    client.files().uploadBuilder("/text.txt").uploadAndFinish(in);
-                    OutputStream downloadFile = new FileOutputStream("C:\\Users\\Hule-Elev\\Documents\\Input.txt");
+                    OutputStream downloadFile = new FileOutputStream("C:\\Users\\Hule-Elev\\Documents\\input.jar");
                     try {
-                        if (metadata.getPathLower().equals("/text.txt")) {
-                            client.files().downloadBuilder("/text.txt").download(downloadFile);
-                            System.out.println("text.txt has been downloaded!");
-                        }else{
-                            System.out.println("text.txt has not been downloaded!");
+                        if (metadata.getPathLower().equals("/text.jar")) {
+                            client.files().downloadBuilder("/text.jar").download(downloadFile);
+                            System.out.println("text.jar has been downloaded!");
                         }
                     } finally {
                         downloadFile.close();
